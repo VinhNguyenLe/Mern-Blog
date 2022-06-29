9348fae46e7ddafce34be6e9d0f97a4a31fad87a
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-29 18:05:39 +0700

    commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Topbar from "./components/Topbar/Topbar";
+import Home from "./pages/Home/Home";
+
+function App() {
+    return (
+        <div className="App">
+            <Topbar />
+            <Home />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import "./Home.scss";
+
+function Home() {
+    return (
+        <div className="home">
+            <Header />
+            {`<HomePage />`}
+        </div>
+    );
+}
+
+export default Home;
